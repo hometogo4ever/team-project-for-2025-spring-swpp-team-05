4036dd430445c72d446ba45c688a95c386307be5
Kwack Minseo <hometogo0625@snu.ac.kr>
2025-05-04 17:24:37 +0900

    Build Setting Update

--- MonsterUniversity/Assets/Editor/BuildScript.cs
+++ MonsterUniversity/Assets/Editor/BuildScript.cs
@@ -1,12 +1,27 @@
 using UnityEditor;
+using UnityEngine;
+using System.Linq;
 
-public class BuildScript
+public class WebGLBuildScript
 {
-    static string[] scenes = { "Assets/Scenes/MainScene.unity" };
-
     [MenuItem("Build/WebGL")]
     public static void BuildWebGL()
     {
+        // 컬러 스페이스 강제 설정
+        PlayerSettings.colorSpace = ColorSpace.Gamma;
+
+        // 빌드 설정에 등록된 씬만 사용
+        var scenes = EditorBuildSettings.scenes
+            .Where(s => s.enabled)
+            .Select(s => s.path)
+            .ToArray();
+
+        if (scenes.Length == 0)
+        {
+            Debug.LogError("No scenes are added to the Build Settings!");
+            return;
+        }
+
         BuildPipeline.BuildPlayer(
             scenes,
             "Build/WebGL",
